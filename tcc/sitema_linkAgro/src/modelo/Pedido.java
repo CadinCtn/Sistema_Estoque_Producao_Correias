2c50b98ef9ac2ef91ef1c4a17513f24b76532137
Cristyan Gabriel Rocha <cristyanrochaoff@gmail.com>
2023-10-04 14:46:09 -0300

    Tela pedidos
    criando classes Dao e Modelo da tela pedidos

--- tcc/sitema_linkAgro/src/modelo/Pedido.java
+++ tcc/sitema_linkAgro/src/modelo/Pedido.java
@@ -4,10 +4,60 @@
  */
 package modelo;
 
+import java.sql.Date;
+
 /**
  *
  * @author Lenovo
  */
 public class Pedido {
+    
+    int id;
+    Date fechamento;
+    Date embarque;
+    String nomeCliente;
+    String observacao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id_pedido) {
+        this.id = id;
+    }
+
+    public Date getFechamento() {
+        return fechamento;
+    }
+
+    public void setFechamento(Date fechamento) {
+        this.fechamento = fechamento;
+    }
+
+    public Date getEmbarque() {
+        return embarque;
+    }
+
+    public void setEmbarque(Date embarque) {
+        this.embarque = embarque;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+    
     
 }
