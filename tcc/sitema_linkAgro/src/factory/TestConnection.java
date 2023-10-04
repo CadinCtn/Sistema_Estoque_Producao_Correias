e6d06c00cfdf907019b0f9e7024fe10974a6314a
Cristyan Gabriel Rocha <cristyanrochaoff@gmail.com>
2023-10-04 15:23:43 -0300

    ConnectionFactory
    Estabelecendo conexão com banco de dados

--- tcc/sitema_linkAgro/src/factory/TestConnection.java
+++ tcc/sitema_linkAgro/src/factory/TestConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 
-public class TestaConexao {
+public class TestConnection {
     
     public static void main(String[] args) throws SQLException {
     
